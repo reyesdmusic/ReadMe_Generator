a22f8eded19022730c1aaba4307cdf035dbb922e
Danny Reyes <reyesdmusic@gmail.com>
2020-04-11 12:08:54 -0400

    prompt user for for username and successful api call

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
 const createReadme = require("./createReadme.js");
+
+function getUserInfo() {
+    inquirer.prompt({
+        message: "What is your GitHub username?",
+        name: "userName"
+      })
+      .then(function ({ userName }) {
+        const gitHubApi = `https://api.github.com/users/${userName}`;
+        axios.get(gitHubApi)
+      .then(function (userInfo) {
+        console.log(userInfo);
+        });
+      });
+    }
+
+    getUserInfo();
